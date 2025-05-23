ee2c8c32d6d55e15616fb025027d1fdfaebfa644
ankitchamke <ankitchamke@gmail.com>
2025-05-23 17:17:58 +0530

    Chapter 5 Done

---
+++ Chapter 5/dict.py
@@ -0,0 +1,21 @@
+marks = {
+    "Chamke":100,
+    "Harry":28,
+    "Kruti":101,
+    106 : "Tsunade"    
+    }
+# print(marks, type(marks))
+# print(marks[0])
+# print(marks["Chamke"])
+
+# METHODS
+# print(marks.items())
+# print(marks.keys())
+# marks.update({"Harry":0})
+# print(marks)
+# print(marks.get("Chamke"))
+# print(marks.get("Cham")) #none
+# print(marks["Cham"]) #error
+# marks.pop(106)
+# marks.popitem()
+# print(marks)
---
+++ Chapter 5/set.py
@@ -0,0 +1,13 @@
+# s = {1,2,3,4}
+# print(s,type(s))
+# a = {}
+# print(type(a))
+# b = set()
+# print(type(b))
+
+s = {1,2,3,4,5,6,7}
+b = {1,9,10,11}
+# s.add(8)
+s.update(b)
+print(s.union(b))
+# print(s.intersection(b))
